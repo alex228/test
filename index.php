5ad40cf539f2648784a64b2fe33b6564dfa06e2e
alex <smokimo@tut.by>
2014-10-21 14:28:51 +0300

    init

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+interface plugin_interface
+{
+    public function call();
+}
+
+class plugin
+{
+    protected $plugins = array();
+
+    public function add_plugin(plugin_interface $plugin) {
+        $this->plugins[] = $plugin;
+        return $this;
+    }
+
+    public function call_plugins() {
+        foreach ($this->plugins as $plugin) {
+            /** @var plugin_interface $plugin_instance */
+            $plugin_instance = new $plugin;
+            $plugin_instance->call();
+        }
+        return $this;
+    }
+}
+
+class plugin_foo implements plugin_interface
+{
+    public function call() {
+        echo 'foo<br/>';
+    }
+}
+
+class plugin_bar implements plugin_interface
+{
+    public function call() {
+        echo 'bar<br/>';
+    }
+}
+
+$plugin = new plugin;
+$plugin->add_plugin(new plugin_foo())
+       ->add_plugin(new plugin_bar())
+       ->call_plugins();
